a42ddcdd61be8545d76a328dea238a45b8337a04
y0ngjunkim <bori3ugo@naver.com>
2021-10-08 01:31:13 +0900

    10월06일 수업내용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-function App() {
-  return <div className="App" />;
-  // return <div>Hello!!!!</div>;
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    console.log('hello');
+    }
+  
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    console.log('add');
+  };
+  minus = () => {
+    console.log('minus');
+  };
+
+  render(){
+    console.log('render');
+    return( 
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button>Add</button>
+        <button>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
